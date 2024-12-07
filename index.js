b90b5da61d950088a989b3b69bf7b133ec86e67b
odhiambootieno <brianmkcnight@gmail.com>
2024-12-07 08:04:22 +0300

    adds

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+
+// Use reduce to sum up the total number of batteries
+const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, 0); // Starting with an initial value of 0
+
+
+// Export the variable if needed
+module.exports = totalBatteries;
